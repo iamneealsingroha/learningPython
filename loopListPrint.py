03441e26197a0d0d243a874cf05cb303f49fbb80
Neeal Singroha <iamneealsingroha@gmail.com>
2026-03-26 11:59:26 +0530

    Create loopListPrint.py

---
+++ loopListPrint.py
@@ -0,0 +1,8 @@
+z = ["mango","apple", "banana", "watermelon","litchi", "orange", "papaya"]
+for i in z:
+    print(i)
+print("Index 4:", z[3])
+print("Index 2:", z[1])
+print("Index 1:", z[0])
+print("Index 5 and 7:", z[4], z[6])
+print("Range 3 to 6 ", z[2:6])
