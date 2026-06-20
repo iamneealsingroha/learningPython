1d9dad98a9c3c04cf90765d5f61e9739a69c7b0f
Neeal Singroha <iamneealsingroha@gmail.com>
2026-06-20 16:52:27 +0530

    Update numberOfFingers.py

--- numberOfFingers.py
+++ numberOfFingers.py
@@ -1,2 +1,11 @@
 import cv2
 import mediapipe as mp
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(
+    static_image_mode = False,
+    max_num_hands = 2,
+    min_detection_confidence = 0.7,
+    min_tracking_confidence = 0.7
+)
+rgb_frame =  cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
